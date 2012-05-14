e84fec851a52cb39ab5b25ca25bb2b864c49bf0e
James Ward <james@jamesward.org>
2012-05-14 10:30:07 -0600

    cleanup for blog

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -4,7 +4,7 @@ import com.example.model.Person;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.neo4j.repository.NamedIndexRepository;
 
-public interface PersonService extends GraphRepository<Person>, NamedIndexRepository<Person> {
+public interface PersonService extends GraphRepository<Person> {
 
     
 }
