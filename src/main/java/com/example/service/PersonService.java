dd50f793bf232968f1e2444909125c79b0cb7695
James Ward <james@jamesward.org>
2012-05-14 11:00:26 -0600

    do not need the NamedIndexRepository

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -2,7 +2,6 @@ package com.example.service;
 
 import com.example.model.Person;
 import org.springframework.data.neo4j.repository.GraphRepository;
-import org.springframework.data.neo4j.repository.NamedIndexRepository;
 
 public interface PersonService extends GraphRepository<Person> {
 
